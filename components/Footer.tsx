5478b3de2be14a836670e68f4d6f99ac541021d1
SMYB5431 <96218527+SMYB5431@users.noreply.github.com>
2025-12-24 10:49:37 +0800

    Update footer to include current year and copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,17 @@
+import React from 'react';
 import { Divider } from '@mantine/core'
 import { pageConfig } from '@/uptime.config'
 
 export default function Footer() {
-  const defaultFooter =
-    '<p style="text-align: center; font-size: 12px; margin-top: 10px;"> Powered by <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">Uptimeflare</a> & <a href="https://github.com/lyc8503" target="_blank">lyc8503</a> </p>'
+  const currentYear = new Date().getFullYear();
 
   return (
     <>
       <Divider mt="lg" />
-      <div dangerouslySetInnerHTML={{ __html: pageConfig.customFooter ?? defaultFooter }} />
+      <div style={{ textAlign: 'center', fontSize: '12px', marginTop: '10px' }}>
+        Copyright © {currentYear} 三木羽冰<br />
+        Powered by <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">Uptimeflare</a>
+      </div>
     </>
   )
 }
